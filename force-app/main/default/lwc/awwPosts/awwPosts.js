f5b16a834c5fd50ecd7e7be77476ab2a87fd7a9c
elier lopez <admin@eliers-MacBook-Pro.local>
2021-08-02 14:35:42 -0700

    changes

--- force-app/main/default/lwc/awwPosts/awwPosts.js
+++ force-app/main/default/lwc/awwPosts/awwPosts.js
@@ -2,37 +2,44 @@
 import { LightningElement, wire } from 'lwc';
 import getAwwList from '@salesforce/apex/AwwController.getAwwList';
 
-const columns = [
-  {label: 'Author', fieldName: 'author_fullname__c', type: 'text'},
-  {label: 'Title', fieldName: 'title__c', type: 'text'},
-  {label: 'Thumbnail', fieldName: 'thumbnail__c', type: 'url'}
-];
-
 const actions = [
   { label: 'Show details', name: 'show_details' }
 ];
 
+const columns = [
+  {
+    type: 'action',
+    typeAttributes: {
+      rowActions: actions,
+      menuAlignment: 'left'
+    }
+  },
+  { label: 'Author', fieldName: 'author_fullname__c', type: 'text' },
+  { label: 'Title', fieldName: 'title__c', type: 'text' },
+  { label: 'Thumbnail', fieldName: 'thumbnail__c', type: 'url' },
+];
+
+
 export default class AwwList extends LightningElement {
   columns = columns;
-  actions = actions;
   record = {};
 
   //hanlde context menu
-  handleRowAction(event) {
+  handleRowActions(event) {
     const actionName = event.detail.action.name;
     const row = event.detail.row;
     switch (actionName) {
-        case 'show_details':
-            this.showRowDetails(row);
-            break;
-        default:
+      case 'show_details':
+        this.showRowDetails(row);
+        break;
+      default:
     }
-}
+  }
 
   //assign value to selected record
   showRowDetails(row) {
     this.record = row;
   }
 
-  @wire(getAwwList)awwPosts;
+  @wire(getAwwList) awwPosts;
 }
